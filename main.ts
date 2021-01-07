4fe0d4722c93a23e9c041cca513d032b122b8b7a
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2021-01-07 14:00:52 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let dot = 2
+basic.clearScreen()
+led.plot(dot, 2)
+
+
+input.onButtonPressed(Button.A, function () {
+    dot += -1
+    basic.clearScreen()
+    led.plot(dot, 2)
+})
+
+
+input.onButtonPressed(Button.B, function () {
+    dot += 1
+    basic.clearScreen()
+    led.plot(dot, 2)
+})
+
+
+basic.forever(function () {
+    if (dot < 0) {
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showString("A wins!")
+    }
+    if (dot > 4) {
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showString("B wins!")
+    }
+})
